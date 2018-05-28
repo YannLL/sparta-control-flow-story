e2055374055fb43175956bc71189266350cdf9a7
Tech-A08 <tech-a08@MacBook-Air-3.local>
2018-05-28 12:56:08 +0100

    Created core file structure

---
+++ js/app.js
@@ -0,0 +1,36 @@
+console.log("is working");
+
+
+// alert('yello');
+// alert('123');
+
+// var name = prompt('name');
+//
+// switch( name ){
+//   case 'Bob':
+//   console.log('Name is Bob');
+//   break;
+//   case 'Martin':
+//   console.log('Name is Martin');
+//   break;
+//   case 'Lucy':
+//   console.log('Name is Lucy');
+//   break;
+//   case 'Jack':
+//   console.log('Name is Jack');
+//   break;
+//   case 'Michael':
+//   console.log('Name is Michael');
+//   break;
+//   default:
+//   console.log('Name doesnt exist');
+// }
+
+// ===== Game text =====
+var introText = "\=====\ A MATRIX-INSPIRED GAME \=====\
+\n This is just another dull day of work. Every day life feels like an arrangement of repetitive tasks and occurrences, neither rewarding or meaningful. \nYou awaken from a nap, as the sun rises -- odd, because the clock shows it is 9:00pm.";
+
+
+// ==========
+
+alert(introText);
