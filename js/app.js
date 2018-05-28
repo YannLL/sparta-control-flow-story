4ed230e99333882a6455232778f791102716b20e
Tech-A08 <tech-a08@MacBook-Air-3.local>
2018-05-28 15:52:27 +0100

    Created basic scripting and player resource mechanics

--- js/app.js
+++ js/app.js
@@ -28,9 +28,38 @@ console.log("is working");
 
 // ===== Game text =====
 var introText = "\=====\ A MATRIX-INSPIRED GAME \=====\
-\n This is just another dull day of work. Every day life feels like an arrangement of repetitive tasks and occurrences, neither rewarding or meaningful. \nYou awaken from a nap, as the sun rises -- odd, because the clock shows it is 9:00pm.";
+\n This story takes place after just another dull day of work. Every day feels like an old sript of the same tasks and occurrences, neither rewarding or meaningful. \n\nYou awaken from a nap, as the sun rises -- odd, because the clock shows it is 9:00pm...";
 
+var messageMorpheus = "Food can wait. You send an encrypted private message to your hacker-buddy, detailing the weird sunrise at 9pm you just witnessed. As soon as you press \'send\' you hear someone knocking on the front door of your apartment."
 
 // ==========
 
+// ===== PLAYER RESOURCES =====
+var tesco = false;
+var takeAway = false;
+var mobilePhone = 0;
+var money = 5;
+// ==========
+
+//===== GAME SCRIPT =====
+// Part - A
 alert(introText);
+
+var msOrFood = prompt ("This is the kind of weird event you love to discuss with your buddy Morpheus. You've never met in real life, but you have spent the last months trading countlss messages, questioning the reality of everything...  \n \n But you're also hungry and have £5, enough for a takeaway or Tesco's \n(a)-Message Morpheus (b)-Tescos (c)-Local take-away");
+
+ switch(msOrFood){
+    case ("a"):
+      alert(messageMorpheus);
+      break;
+
+    case ("b"):
+      alert(" ");
+      break;
+    case ("c"):
+      alert("You message Morpheus");
+      break;
+
+    default:
+      alert("Player input failed - Please restart the game");
+      break;
+  }
